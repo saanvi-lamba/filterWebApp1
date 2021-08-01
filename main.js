0c17ee7f0ec01f02435484ca6a5a53b1851f5192
saanvi-lamba <82785264+saanvi-lamba@users.noreply.github.com>
2021-08-01 18:41:44 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save('FilterWebAppImage.png');
+}
